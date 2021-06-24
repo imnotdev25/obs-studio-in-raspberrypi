4c5a71786bf50ca5a92f166888d2fb3fccc32228
imnotdev25 <85677268+imnotdev25@users.noreply.github.com>
2021-06-24 12:05:30 +0530

    Update obs.sh

--- obs.sh
+++ obs.sh
@@ -1,5 +1,5 @@
 echo ""
-echo "Checking memory size.........."
+echo "Checking memory size.......... GitHub @imnotdev25"
 Totalmem=$(cat /proc/meminfo|grep MemTotal|grep -o '[0-9]*')
 if (($Totalmem > 3500000)); then
     echo ""
@@ -16,7 +16,7 @@ else
     sudo swapon
 fi
 echo ""
-echo "Compiling FFMPEG.........."
+echo "Compiling FFMPEG.......... GitHub @imnotdev25"
 echo ""
 sudo apt-get -y install autoconf automake build-essential cmake doxygen git graphviz imagemagick libasound2-dev libass-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libfreetype6-dev libgmp-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libopus-dev librtmp-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev libsnappy-dev libsoxr-dev libssh-dev libssl-dev libtool libv4l-dev libva-dev libvdpau-dev libvo-amrwbenc-dev libvorbis-dev libwebp-dev libx264-dev libx265-dev libxcb-shape0-dev libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev libxml2-dev lzma-dev meson nasm pkg-config python3-dev python3-pip texinfo wget yasm zlib1g-dev libdrm-dev
 cd /home/pi/
@@ -112,7 +112,7 @@ cd ~/ffmpeg-libraries/FFmpeg \
   && sudo make install
 
 echo ""
-echo "Installing OBS.........."
+echo "Installing OBS.......... github @imnotdev25"
 echo ""
 sudo apt-get update
 sudo apt-get -y install build-essential checkinstall cmake git libmbedtls-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libcurl4-openssl-dev libfontconfig1-dev libfreetype6-dev libgl1-mesa-dev libjack-jackd2-dev libjansson-dev libluajit-5.1-dev libpulse-dev libqt5x11extras5-dev libspeexdsp-dev libswresample-dev libswscale-dev libudev-dev libv4l-dev libvlc-dev libx11-dev libx11-xcb1 libx11-xcb-dev libxcb-xinput0 libxcb-xinput-dev libxcb-randr0 libxcb-randr0-dev libxcb-xfixes0 libxcb-xfixes0-dev libx264-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcomposite-dev libxinerama-dev pkg-config python3-dev qtbase5-dev libqt5svg5-dev swig
